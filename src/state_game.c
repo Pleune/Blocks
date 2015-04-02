4f5280abd10cffbd6b4ff36662bb18034e646bd8
Mitchell Pleune <mitchpleune@gmail.com>
2015-04-02 17:35:30 -0400

    functionized raypicking
    It's in another c file now too

--- src/state_game.c
+++ src/state_game.c
@@ -13,6 +13,9 @@
 
 #include "world.h"
 
+
+#include "state_game/blockpick.c"
+
 uint32_t ticks = 0;
 
 int windoww, windowh;
@@ -294,75 +297,13 @@ state_game_run()
 		{
 			if(e.button.button == SDL_BUTTON_LEFT)
 			{
-				int3_t p;
-				p.x = (int)floorf(pos.x);
-				p.y = (int)floorf(pos.y);
-				p.z = (int)floorf(pos.z);
-
-				vec3_t b;
-				b.x = pos.x - floorf(pos.x);
-				b.y = pos.y - floorf(pos.y);
-				b.z = pos.z - floorf(pos.z);
-
-				vec3_t r = forwardcamera;
-
-				int posx = r.x > 0 ? 1 : 0;
-				int posy = r.y > 0 ? 1 : 0;
-				int posz = r.z > 0 ? 1 : 0;
-
-				float dirx = posx ? 1 : -1;
-				float diry = posy ? 1 : -1;
-				float dirz = posz ? 1 : -1;
-
-				vec3_t rt;
-				rt.y = r.y / r.x;
-				rt.z = r.z / r.x;
-				float deltax = sqrtf(1 + rt.y*rt.y + rt.z*rt.z);
-				rt.x = r.x / r.y;
-				rt.z = r.z / r.y;
-				float deltay = sqrtf(rt.x*rt.x + 1 + rt.z*rt.z);
-				rt.x = r.x / r.z;
-				rt.y = r.y / r.z;
-				float deltaz = sqrtf(rt.x*rt.x + rt.y*rt.y + 1);
-
-				block_t block;
-				block.id = 1;
-
-				float maxx = deltax * (posx ? (1 - b.x) : b.x);
-				float maxy = deltay * (posy ? (1 - b.y) : b.y);
-				float maxz = deltaz * (posz ? (1 - b.z) : b.z);
-
-				int i;
-				for(i=0; i<1000; i++)
-				{
-					if(maxx < maxy && maxx < maxz)
-					{
-						maxx += deltax;
-						p.x += dirx;
-						if(block_issolid(world_getblock(p.x,p.y,p.z,0)))
-							{p.x -= dirx; break;}
-					}
-					else if(maxy < maxx && maxy < maxz)
-					{
-						maxy += deltay;
-						p.y += diry;
-						if(block_issolid(world_getblock(p.x,p.y,p.z,0)))
-							{p.y -= diry; break;}
-					}
-					else if(maxz < maxx && maxz < maxy)
-					{
-						maxz += deltaz;
-						p.z += dirz;
-						if(block_issolid(world_getblock(p.x,p.y,p.z,0)))
-							{p.z -= dirz; break;}
-					}
-				}
-				world_setblock(p.x, p.y, p.z, block, 0);
-				break;
+				block_t b;
+				b.id = 1;
+				rayadd(pos, forwardcamera, b, 1);
 			}
 			else if(e.button.button == SDL_BUTTON_RIGHT)
 			{
-
+				raydel(pos, forwardcamera);
 			}
 		}
 		else if(e.type == SDL_WINDOWEVENT)
