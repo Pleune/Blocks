311af82114e2d707e6629e6460ce0e3b02c85aa2
Mitchell Pleune <mitchpleune@gmail.com>
2016-04-06 16:30:54 -0400

    Forgot sand

--- src/block.c
+++ src/block.c
@@ -1,13 +1,15 @@
 #include "block.h"
 #include "world.h"
 
-const blockdata_t blockinfo[(enum block_id) ERR] = {
+const blockdata_t blockinfo[(enum block_id) ERR + 1] = {
 	[AIR] = {0, {0,0,0}, "Air"},
 	[STONE] = {1, {0.2,0.2,0.22}, "Stone"},
 	[DIRT] = {1, {0.185,0.09,0.05}, "Dirt"},
 	[GRASS] = {1, {0.05,0.27,0.1}, "Grass"},
+	[SAND] = {1, {0.28,0.3,0.15}, "Sand"},
 	[BEDROCK] = {1, {0.1,0.1,0.1}, "Hard Stone"},
-	[WATER] = {1, {0.08,0.08,0.3}, "Water"}
+	[WATER] = {1, {0.08,0.08,0.3}, "Water"},
+	[ERR] = {0, {1,0,0}, "Error"}
 };
 
 
